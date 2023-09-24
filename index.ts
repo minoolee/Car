745dc705c86a22a6c98689dbf68b6f3d8a526f85
minoolee <yaqoubiamina@gmail.com>
2023-09-24 23:49:52 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,104 @@
+// Auto scout
+
+
+interface Car {
+  CarId: number,
+  CarBrand: string,
+  CarModel: string,
+  CarBodyType: string,
+  CarPrice: number,
+  CarFuelType: string,
+  CarYear: number,
+  CarAvailable: boolean
+}
+
+const bmwI7: Car ={
+  CarId: 1,
+  CarBrand: "BMW",
+  CarModel: "i7",
+  CarBodyType: "COUPE",
+  CarPrice: 245,
+  CarFuelType: "Gasoline",
+  CarYear: 2002,
+  CarAvailable: true 
+}
+
+const mercedesH3: Car ={
+  CarId: 2,
+  CarBrand: "Mercedes",
+  CarModel: "h3",
+  CarBodyType: "SPORTS CAR",
+  CarPrice: 333,
+  CarFuelType: "Diesel",
+  CarYear: 2003,
+  CarAvailable: false 
+}
+ 
+
+  const hundaK3: Car ={
+    CarId: 3,
+    CarBrand: "Honda",
+    CarModel: "k3",
+    CarBodyType: "CONVERTIBLE",
+    CarPrice: 777,
+    CarFuelType: "Diesel",
+    CarYear: 2007,
+    CarAvailable: true
+  }
+
+    const  bmwA5: Car = {
+      CarId: 4,
+      CarBrand: "BMW",
+      CarModel: "a5",
+      CarBodyType: "HATCHBACK",
+      CarPrice: 555,
+      CarFuelType: "Ethanol",
+      CarYear: 2005,
+      CarAvailable: true
+    }
+
+class CarMarket < S > {
+  constructor(
+    private id: number,
+    readonly brand: S,
+    public cars: Car[] = [],
+    public addsCar: Car[] = []
+    ) {} 
+    addCar(carBrand: string, carBodyType: string ) {
+    const addCar = cars.find((carItem) => carItem.CarBrand === carBrand && carItem.CarBodyType === carBodyType )
+    if (addCar){
+      this.addsCar.push(addCar)
+    } else {
+      console.log("Car not found");
+    }
+    
+   }
+   showStartPage(): string {
+    return `Welcome to the car market ${this.brand}`
+   }
+   showAllCars(): string[] {
+    const allCarsInfo = this.cars.map(car => `Brand: ${car.CarBrand}\nModel: ${car.CarModel} \n Car Type: ${car.CarBodyType} \n Price: ${car.CarPrice} \n Car Fuel: ${car.CarFuelType} \n Car Year: ${car.CarYear} \n Car Avaolable: ${car.CarAvailable}` );
+    return allCarsInfo;
+      
+    };
+   }
+    
+
+const cars =[bmwI7, mercedesH3, hundaK3, bmwA5]
+
+const autoScout = new CarMarket < string > ( 7, "Auto Scout", cars)
+
+//autoScout.addCar(cars)
+console.log(autoScout.showStartPage);
+console.log("All Cars: ");
+console.log(autoScout.showAllCars());
+autoScout.addCar("BMW","HATCHBACK")
+console.log("Added Cars: ");
+console.log(autoScout.addsCar);
+
+
+
+
+
+
+
